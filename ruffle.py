5d531bf5e5a68e0100947d9873447bd634df08de
Natalia Rodríguez Calvo <natalia.rodriguez.calvo@udc.es>
2024-06-07 22:04:11 +0200

    cogs added

--- ruffle.py
+++ ruffle.py
@@ -2,3 +2,10 @@ import discord
 import dotenv
 dotenv.load_dotenv()
 token = str(os.getenv("TOKEN"))
+cogs_list = [
+    'drawwinners',
+    'checkpastruffles'
+]
+
+for cog in cogs_list:
+    bot.load_extension(f'cogs.{cog}')
